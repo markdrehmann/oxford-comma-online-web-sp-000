ee2105dcc0b6dcca203b8cf2912faa9af25cc042
Mark Drehmann <markdrehmann@gmail.com>
2020-01-18 17:53:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
-  final_item = array.last
   if array.size >= 3
     array[-2,-1].join(", and ")
 
   elsif array.size == 2
     array.join(" and ")
+  else
+    array.join
   end
 end
