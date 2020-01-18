5e747678b9f8f53147688c25c5dcb57d3eec2d6d
Mark Drehmann <markdrehmann@gmail.com>
2020-01-18 17:49:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   final_item = array.last
   if array.size >= 3
-    string = array.join(", ")
+    string = array.join(", and ")
     string
   elsif array.size == 2
     array.join(" and ")
