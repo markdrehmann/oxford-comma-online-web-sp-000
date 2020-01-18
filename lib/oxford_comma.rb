4a256db3fe6143f219d3406466852a4ae4a48a2f
Mark Drehmann <markdrehmann@gmail.com>
2020-01-18 17:45:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   final_item = array.last
   if array.size >= 3
     string = array.join(", ")
-    string.insert(-2, "and ")
+    string
   elsif array.size == 2
     array.join(" and ")
   else
