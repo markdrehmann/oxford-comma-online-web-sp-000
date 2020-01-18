6075cb01810b8d5d669786294bca47bc7aea7bd2
Mark Drehmann <markdrehmann@gmail.com>
2020-01-18 17:56:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size >= 3
+    final = array.pop
+    array.join(", ") + ", and #{final}"
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array.join
+  end
 end
